451f61f142b91efd164bb79f32697fb91fa2d438
arisas <arisashiraishi@outlook.com>
2025-02-03 17:34:04 +0900

    gmm

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,28 +1,29 @@
-import { PortableText } from "next-sanity";
-import { type PortableTextBlock } from "next-sanity";
-import { notFound } from "next/navigation";
-import { sanityFetch } from "@/sanity/lib/fetch";
-import { aboutQuery } from "@/sanity/lib/queries";
+// import { PortableText } from "next-sanity";
+// import { type PortableTextBlock } from "next-sanity";
+// import { notFound } from "next/navigation";
+// import { sanityFetch } from "@/sanity/lib/fetch";
+// import { aboutQuery } from "@/sanity/lib/queries";
 
-export async function generateStaticParams() {
-  return await sanityFetch({
-    query: aboutQuery,
-  });
-}
+// export async function generateStaticParams() {
+//   return await sanityFetch({
+//     query: aboutQuery,
+//   });
+// }
 
 export default async function Page() {
-  const about = await sanityFetch({ query: aboutQuery });
+  //   const about = await sanityFetch({ query: aboutQuery });
 
-  if (!about?._id) {
-    return notFound();
-  }
+  //   if (!about?._id) {
+  //     return notFound();
+  //   }
 
   return (
     <div className="container mx-auto px-5">
       <article>
-        {about.content?.length && (
+        About page under construction
+        {/* {about.content?.length && (
           <PortableText value={about.content as PortableTextBlock[]} />
-        )}
+        )} */}
       </article>
       <aside></aside>
     </div>
