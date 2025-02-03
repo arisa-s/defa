29fad74d2ad02fb41685f6948824fe39e869a76b
arisas <arisashiraishi@outlook.com>
2025-02-03 20:39:45 +0900

    maybe about page is good now too

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,29 +1,40 @@
-// import { PortableText } from "next-sanity";
-// import { type PortableTextBlock } from "next-sanity";
-// import { notFound } from "next/navigation";
-// import { sanityFetch } from "@/sanity/lib/fetch";
-// import { aboutQuery } from "@/sanity/lib/queries";
+import { PortableText } from "next-sanity";
+import type { Metadata } from "next";
+import { type PortableTextBlock } from "next-sanity";
+import { notFound } from "next/navigation";
+import { sanityFetch } from "@/sanity/lib/fetch";
+import { aboutQuery, siteSettingsQuery } from "@/sanity/lib/queries";
 
-// export async function generateStaticParams() {
-//   return await sanityFetch({
-//     query: aboutQuery,
-//   });
-// }
+export async function generateStaticParams() {
+  return await sanityFetch({
+    query: aboutQuery,
+  });
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const site = await sanityFetch({
+    query: siteSettingsQuery,
+    stega: false,
+  });
+
+  return {
+    description: `About ${site?.title}`,
+  } satisfies Metadata;
+}
 
 export default async function Page() {
-  //   const about = await sanityFetch({ query: aboutQuery });
+  const [about] = await Promise.all([sanityFetch({ query: aboutQuery })]);
 
-  //   if (!about?._id) {
-  //     return notFound();
-  //   }
+  if (!about?._id) {
+    return notFound();
+  }
 
   return (
     <div className="container mx-auto px-5">
       <article>
-        About page under construction
-        {/* {about.content?.length && (
+        {about.content?.length && (
           <PortableText value={about.content as PortableTextBlock[]} />
-        )} */}
+        )}
       </article>
       <aside></aside>
     </div>
