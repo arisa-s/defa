a0f2043663b0ad3118222ec4efc17a1353e4e87e
arisas <arisashiraishi@outlook.com>
2025-03-12 22:41:56 +0900

    piesy

--- constants/links.ts
+++ constants/links.ts
@@ -2,8 +2,8 @@
 
 export const LINKS = [
   { href: "/", label: "Home" },
-  { href: "/about", label: "About" },
   { href: "/project", label: "Projects" },
   { href: "/publication", label: "Publications" },
   { href: "/film", label: "Films" },
+  { href: "/about", label: "About" },
 ];
